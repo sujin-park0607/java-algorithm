4e5c2f78c6c3723a03f4788861aa08b2b3e14fba
sujin0607 <goweg0607@gmail.com>
2022-10-21 10:19:08 +0900

    프로그래머스 괄호 풀기

--- src/main/java/com/likelion/programmers/_12909.java
+++ src/main/java/com/likelion/programmers/_12909.java
@@ -5,31 +5,30 @@ import java.util.Stack;
 
 public class _12909 {
     boolean solution(String s) {
-        while(s.indexOf("()") >= 0){
-            s = s.replace("()","");
+//        while(s.indexOf("()") >= 0){
+//            s = s.replace("()","");
+//        }
+//        return s.length() == 0;
+
+
+        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : arr) {
+            if(c == '('){
+                stack.push('(');
+            }else{
+                if(stack.isEmpty() || stack.peek() ==')'){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
         }
-        return s.length() == 0;
+
+        if(stack.isEmpty() == false){
+            return false;
+        }
+        return true;
     }
 }
-
-        //        char[] arr = s.toCharArray();
-//        Stack<String> stack = new Stack<>();
-//
-//        for (char c : arr) {
-//            System.out.println(stack);
-//            if(c == '('){
-//                stack.push("(");
-//            }else{
-//                if(stack.size() != 0 && stack.peek().equals('(')){
-//                    stack.pop();
-//                }else{
-//                    return false;
-//                }
-//            }
-//        }
-//
-//        if(stack.size() != 0){
-//            return false;
-//        }
-//        return true;
-
