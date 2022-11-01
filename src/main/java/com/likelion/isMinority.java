55acdbfdfd604bb15af267f10e2cd07c96f90f03
sujin0607 <goweg0607@gmail.com>
2022-11-01 09:32:08 +0900

    Feat: 추가 소수찾기 solution2 메서드 구현
    2~ (num/2) 까지 나누어서 나머지가 0이 안나오면 소수

--- src/main/java/com/likelion/isMinority.java
+++ src/main/java/com/likelion/isMinority.java
@@ -10,14 +10,21 @@ public class isMinority {
         return true;
     }
 
-
+    public boolean solution2(int num){
+        for(int i=2; i<num/2; i++){
+            if(num%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         isMinority minority = new isMinority();
         int[] number = {13, 17, 19, 23};
 
         for (int num : number) {
-            System.out.println(minority.solution1(num));
+            System.out.println(minority.solution2(num));
         }
     }
 
