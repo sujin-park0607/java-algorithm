38667640b8d1b5ed7bf57b852eca8166b1b811d6
sujin0607 <goweg0607@gmail.com>
2022-11-02 10:45:46 +0900

    error: IndexOutof에러가 남

--- src/main/java/com/likelion/Eratos.java
+++ src/main/java/com/likelion/Eratos.java
@@ -1,23 +1,35 @@
 package com.likelion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Eratos {
-    public static void main(String[] args) {
+    public int solution(int n){
         List<Integer> numArr = new ArrayList<>();
+        Boolean[] checks = new Boolean[n];
         //리스트 만들기
-        for(int i=0; i<18; i++) numArr.add(i);
+        for(int i=2; i<n; i++){
+            numArr.add(i);
+            checks[i] = true;
+        }
 
-
-        for(int num=2; num<7; num++){
-            for(int i=0; i<numArr.size(); i++){
-                if(numArr.get(i) !=num && numArr.get(i) %num == 0){
-                    numArr.remove(i);
+        for(int i=2; i*i <=n; i++){
+            for(int j=0; j<numArr.size(); j++){
+                if(numArr.get(j) > i && numArr.get(j) % i == 0 && checks[j]==true){
+                    checks[j] = false;
                 }
             }
-            System.out.println(numArr.size());
-            System.out.println(numArr);
         }
+        for (int i=2; i<n; i++){
+            if(checks[i] == true){
+                System.out.printf("%d ",numArr.get(i));
+            }
+        }
+        return numArr.size();
+    }
+    public static void main(String[] args) {
+        Eratos isPrime = new Eratos();
+        System.out.println(isPrime.solution(50));
     }
 }
