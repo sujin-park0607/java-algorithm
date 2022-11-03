e3650e1133766d39f7a2d62cb8042a9c59bcff1b
sujin0607 <goweg0607@gmail.com>
2022-11-03 10:05:45 +0900

    feat: Eratos 강사님방법

--- src/main/java/com/likelion/Eratos2.java
+++ src/main/java/com/likelion/Eratos2.java
@@ -1,12 +1,17 @@
 package com.likelion;
 
+import java.util.Arrays;
+
 public class Eratos2 {
     public static void main(String[] args) {
         int n = 50;
-        for(int i=2, k=2; i<n ; i++, k+=2){
-            System.out.println(i +","+ k);
+        int[] nums = new int[n-1];
+        for(int i=0; i<nums.length; i++)nums[i] = i+2;
+        boolean[] checks = new boolean[nums.length];
+        Arrays.fill(checks, true);
+
+        for(int i=0; i<nums.length ; i++){
+            nums[i] = i+2;
         }
-
     }
-
 }
