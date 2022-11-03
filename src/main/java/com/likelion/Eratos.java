f2e926b711341ca918165412a80c484601cd936d
sujin0607 <goweg0607@gmail.com>
2022-11-03 09:24:38 +0900

    error: IndexOutof에러

--- src/main/java/com/likelion/Eratos.java
+++ src/main/java/com/likelion/Eratos.java
@@ -5,30 +5,39 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Eratos {
-    public int solution(int n){
+    public int solution(int n) {
         List<Integer> numArr = new ArrayList<>();
         Boolean[] checks = new Boolean[n];
         //리스트 만들기
-        for(int i=2; i<n; i++){
+        for (int i = 2; i < n; i++) {
             numArr.add(i);
-            checks[i] = true;
         }
+        Arrays.fill(checks,true);
+//        System.out.println(numArr.size()); 48
+//        System.out.println(checks.length); 50
 
-        for(int i=2; i*i <=n; i++){
-            for(int j=0; j<numArr.size(); j++){
-                if(numArr.get(j) > i && numArr.get(j) % i == 0 && checks[j]==true){
-                    checks[j] = false;
+        for (int i = 2; i * i <= n; i++) {
+            for (int j = 0; j < numArr.size(); j++) {
+                System.out.println("numArr.get(j): "+numArr.get(j));
+                System.out.println("checks[j - 2]: "+checks[j - 2]);
+
+                if (numArr.get(j) > i && numArr.get(j) % i == 0 && checks[j - 2] == true) {
+                    checks[numArr.get(j - 2)] = false;
                 }
             }
         }
-        for (int i=2; i<n; i++){
-            if(checks[i] == true){
-                System.out.printf("%d ",numArr.get(i));
+        for (int i = 0; i < n; i++) {
+            System.out.println(checks[i]);
+            System.out.println(numArr.get(i));
+            if (checks[i] == true) {
+                System.out.printf("%d ", numArr.get(i+2));
+//            }
             }
         }
         return numArr.size();
     }
-    public static void main(String[] args) {
+
+    public static void main (String[]args){
         Eratos isPrime = new Eratos();
         System.out.println(isPrime.solution(50));
     }
