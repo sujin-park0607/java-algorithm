7ad05eea70c99f195490fdf1dc8f1cd7c4e61022
sujin0607 <goweg0607@gmail.com>
2022-11-07 09:49:58 +0900

    feat: 나누어 떨어지는 숫자 배열 완료

---
+++ src/main/java/com/likelion/programmers/_12910.java
@@ -0,0 +1,27 @@
+package com.likelion.programmers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class _12910 {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+        List<Integer> answer = new ArrayList<>();
+
+        for (int num : arr) {
+            if(num%divisor == 0){
+                answer.add(num);
+            }
+        }
+        if(answer.size() == 0){
+            answer.add(-1);
+        }
+        Collections.sort(answer);
+        answer.stream().mapToInt(i->i).toArray();
+        System.out.println(answer);
+    }
+
+
+}
