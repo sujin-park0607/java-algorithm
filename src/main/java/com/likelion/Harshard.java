ab3c855604e78891866efc36d4753d350fe11bcf
sujin0607 <goweg0607@gmail.com>
2022-11-07 09:39:02 +0900

    refactor: 공백 수정

--- src/main/java/com/likelion/Harshard.java
+++ src/main/java/com/likelion/Harshard.java
@@ -2,9 +2,9 @@ package com.likelion;
 
 public class Harshard {
     //자릿수를 더하는 함수
-    public int sumOfDigit(int number){
+    public int sumOfDigit(int number) {
         int sum = 0;
-        while(number > 0){
+        while (number > 0) {
             sum += number % 10;
             number /= 10;
         }
@@ -12,17 +12,17 @@ public class Harshard {
     }
 
     //하샤드인지 확인
-    public boolean isHarshard(int number, int sum){
-        if(number % sum == 0) return true;
+    public boolean isHarshard(int number, int sum) {
+        if (number % sum == 0) return true;
         else return false;
     }
 
     public static void main(String[] args) {
         Harshard hs = new Harshard();
-        int x = 135;
+        int x = 11;
 
         int sum = hs.sumOfDigit(x);
-        System.out.println(hs.isHarshard(x,sum));
+        System.out.println(hs.isHarshard(x, sum));
     }
 
 }
