479b9d4c26773c273f999a9c8a6106498155ce83
sujin0607 <goweg0607@gmail.com>
2022-11-14 10:21:37 +0900

    feat: 에라토스테네스의 체 2를 제외한 2의배수 0으로 만들기

--- src/main/java/com/likelion/Eratos3.java
+++ src/main/java/com/likelion/Eratos3.java
@@ -1,8 +1,7 @@
 package com.likelion;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+
 
 public class Eratos3 {
     public static void main(String[] args) {
@@ -10,13 +9,11 @@ public class Eratos3 {
         int[] arr = new int[N - 1];
         System.out.println(arr.length);
 
-        for(int i=0; i<arr.length; i++){
-            arr[i] = i + 2;
-        }
+        for (int i = 0; i < arr.length; i++) arr[i] = i + 2;
 
-        for(int j=0; j<arr.length; j++){
-            if(arr[j] == 2) continue;
-            if(arr[j] % 2 == 0){
+
+        for (int j = 1; j < arr.length; j++) {
+            if (j % 2 == 0) {
                 arr[j] = 0;
             }
         }
