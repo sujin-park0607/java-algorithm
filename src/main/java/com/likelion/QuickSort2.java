4f01bbc574de7fb3022344b2a19b36efcb8bb470
sujin0607 <goweg0607@gmail.com>
2022-11-16 10:49:45 +0900

    feat: 두 그룹으로 나누기

--- src/main/java/com/likelion/QuickSort2.java
+++ src/main/java/com/likelion/QuickSort2.java
@@ -1,24 +1,26 @@
 package com.likelion;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class QuickSort2 {
-    public int standard(int[] arr){
-        return arr.length/2;
-    }
 
     public static void main(String[] args) {
-        QuickSort2 quickSort = new QuickSort2();
 
         int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
-        int pivot = quickSort.standard(arr);
+        int pivot = arr.length/2;
 
         int left = 0;
         int right = arr.length-1;
 
         while(arr[left] < arr[pivot]) left ++;
         while(arr[right] > arr[pivot]) right --;
+        if(left <= right){
+            int temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+            left ++;
+            right --;
+        }
         if(left <= right){
             int temp = arr[right];
             arr[right] = arr[left];
