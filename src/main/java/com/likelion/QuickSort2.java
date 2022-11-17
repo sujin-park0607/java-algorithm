bfa597f9323f71727872eacda6b4035d03897b29
sujin0607 <goweg0607@gmail.com>
2022-11-17 09:31:03 +0900

    refactor: 로직 분리 중

--- src/main/java/com/likelion/QuickSort2.java
+++ src/main/java/com/likelion/QuickSort2.java
@@ -3,30 +3,43 @@ package com.likelion;
 import java.util.Arrays;
 
 public class QuickSort2 {
+    public int[] sort(int[] arr, int left, int right) {
+        int pivot = arr.length / 2;
+        while (arr[left] < arr[pivot]) left++;
+        while (arr[right] > arr[pivot]) right--;
+        if (left <= right) {
+            int temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+            left++;
+            right--;
+        }
+
+    }
 
     public static void main(String[] args) {
 
         int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
-        int pivot = arr.length/2;
+        int pivot = arr.length / 2;
 
         int left = 0;
-        int right = arr.length-1;
+        int right = arr.length - 1;
 
-        while(arr[left] < arr[pivot]) left ++;
-        while(arr[right] > arr[pivot]) right --;
-        if(left <= right){
+        while (arr[left] < arr[pivot]) left++;
+        while (arr[right] > arr[pivot]) right--;
+        if (left <= right) {
             int temp = arr[right];
             arr[right] = arr[left];
             arr[left] = temp;
-            left ++;
-            right --;
+            left++;
+            right--;
         }
-        if(left <= right){
+        if (left <= right) {
             int temp = arr[right];
             arr[right] = arr[left];
             arr[left] = temp;
-            left ++;
-            right --;
+            left++;
+            right--;
         }
 
         System.out.println(Arrays.toString(arr));
