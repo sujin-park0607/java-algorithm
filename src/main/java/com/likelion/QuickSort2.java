d5b20474c011be5379155066db9b6183a4a064e7
sujin0607 <goweg0607@gmail.com>
2022-11-17 09:49:31 +0900

    refactor: QuickSort 재귀 도전중

--- src/main/java/com/likelion/QuickSort2.java
+++ src/main/java/com/likelion/QuickSort2.java
@@ -1,9 +1,13 @@
 package com.likelion;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class QuickSort2 {
     public int[] sort(int[] arr, int left, int right) {
+        if (arr.length <= 0){
+            return arr;
+        }
         int pivot = arr.length / 2;
         while (arr[left] < arr[pivot]) left++;
         while (arr[right] > arr[pivot]) right--;
@@ -14,26 +18,23 @@ public class QuickSort2 {
             left++;
             right--;
         }
+        int[] leftArr = IntStream.range(0, right)
+                .map(i-> arr[i]).toArray();
+        sort(leftArr, 0, right);
 
+        int[] rightArr = IntStream.range(left, arr.length-1)
+                .map(i-> arr[i]).toArray();
+        sort(rightArr, left, arr.length-1);
     }
 
     public static void main(String[] args) {
+        QuickSort2 quickSort = new QuickSort2();
 
         int[] arr = {20, 18, 5, 19, 40, 50, 5, 25};
-        int pivot = arr.length / 2;
-
         int left = 0;
         int right = arr.length - 1;
 
-        while (arr[left] < arr[pivot]) left++;
-        while (arr[right] > arr[pivot]) right--;
-        if (left <= right) {
-            int temp = arr[right];
-            arr[right] = arr[left];
-            arr[left] = temp;
-            left++;
-            right--;
-        }
+        quickSort.sort(arr, left, right);
         if (left <= right) {
             int temp = arr[right];
             arr[right] = arr[left];
