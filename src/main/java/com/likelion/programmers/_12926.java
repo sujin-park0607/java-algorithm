0a96ffc422403569900bde4c9d8537d725c02d92
sujin0607 <goweg0607@gmail.com>
2022-11-18 10:16:49 +0900

    feat: programmers 12926 문제 아스키코드를 활용하여 푸는 중

--- src/main/java/com/likelion/programmers/_12926.java
+++ src/main/java/com/likelion/programmers/_12926.java
@@ -1,26 +1,28 @@
 package com.likelion.programmers;
 
 public class _12926 {
-        public String solution(String s, int n) {
-            String answer = "";
-
-            for(int i=0; i<s.length(); i++){
-                int num = (int)s.charAt(i)+n;
-                if(num >= 91 && num <= 96) num -= 26;
-                if(num>= 122) num -= 26;
-                if(num-n == 32) num -= n;
-
-
-                char ch = (char)num;
-
-                answer += ch;
-            }
-            System.out.println(answer);
-           return answer;
+    public String solution(String s, int n) {
+        String answer = "";
+        if (n >= 26) {
+            n %= 26;
         }
+
+        for (int i = 0; i < s.length(); i++) {
+            int num = (int) s.charAt(i) + n;
+            if ((num >= 91 && num <= 96) || num >= 122) num -= 26;
+            if (num - n == 32) num = 32;
+
+            char ch = (char) num;
+
+            answer += ch;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
 
     public static void main(String[] args) {
         _12926 solution = new _12926();
-        solution.solution("a B z",4);
+        solution.solution("AB", 1);
     }
 }
