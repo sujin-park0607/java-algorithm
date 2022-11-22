f5152825fda182f5de4439c20a741c7add8be650
sujin0607 <goweg0607@gmail.com>
2022-11-22 09:43:24 +0900

    feat: 재귀 별찍기

---
+++ src/main/java/com/likelion/PrintStar.java
@@ -0,0 +1,17 @@
+package com.likelion;
+
+import java.util.Scanner;
+
+public class PrintStar {
+    public static void printStar(int n){
+        if(n<=0) return;
+        printStar(n-1);
+        System.out.printf("*");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printStar(n);
+    }
+}
