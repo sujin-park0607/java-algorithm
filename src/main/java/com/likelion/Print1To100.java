e728e2bb6c45de1714cf2f7b8dadd7cc6a200e64
sujin0607 <goweg0607@gmail.com>
2022-11-22 09:12:58 +0900

    feat: 재귀 호출 시도

---
+++ src/main/java/com/likelion/Print1To100.java
@@ -0,0 +1,12 @@
+package com.likelion;
+
+public class Print1To100 {
+    public static void print1To100(int n){
+        System.out.println(n);
+        print1To100(n);
+    }
+
+    public static void main(String[] args) {
+        print1To100(1);
+    }
+}
