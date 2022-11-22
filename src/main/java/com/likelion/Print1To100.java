eaafcdc7d1d187081fc52ca16bf0dfecb648bc40
sujin0607 <goweg0607@gmail.com>
2022-11-22 09:24:48 +0900

    feat: 재귀 탈출 조건 걸기

--- src/main/java/com/likelion/Print1To100.java
+++ src/main/java/com/likelion/Print1To100.java
@@ -1,9 +1,11 @@
 package com.likelion;
 
 public class Print1To100 {
-    public static void print1To100(int n){
-        System.out.println(n);
-        print1To100(n);
+
+    public static void print1To100(int num){
+        if(num > 100) return;
+        System.out.println(num);
+        print1To100(num +1);
     }
 
     public static void main(String[] args) {
