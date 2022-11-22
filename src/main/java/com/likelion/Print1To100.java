f86d354731ffe756947adefcd1c3410368bc1b70
sujin0607 <goweg0607@gmail.com>
2022-11-22 09:33:41 +0900

    feat: 재귀 arr 배열 더하기

--- src/main/java/com/likelion/Print1To100.java
+++ src/main/java/com/likelion/Print1To100.java
@@ -2,13 +2,19 @@ package com.likelion;
 
 public class Print1To100 {
 
-    public static void print1To100(int num){
-        if(num > 100) return;
-        System.out.println(num);
-        print1To100(num +1);
+    public static void print1To100(int n, int sum){
+        int[] arr = {7, 3, 2, 9};
+
+        if(n > arr.length-1){
+            System.out.println(sum);
+            return;
+        }
+
+        print1To100(n+1, sum + arr[n]);
     }
 
     public static void main(String[] args) {
-        print1To100(1);
+
+        print1To100(0, 0);
     }
 }
