d4b2e8ee80015f697ec58c3c923d8f5b9a397069
sujin0607 <goweg0607@gmail.com>
2022-11-23 10:46:05 +0900

    refactor: 메서드 분리 수정

--- src/main/java/com/likelion/Fibonacci.java
+++ src/main/java/com/likelion/Fibonacci.java
@@ -5,16 +5,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-
+    public static void fibonacci(int num) {
         List<Integer> fibonacci = new ArrayList<>();
         fibonacci.add(1);
         fibonacci.add(1);
-        for(int i=2; i<num; i++){
-            fibonacci.add(fibonacci.get(i-1) + fibonacci.get(i-2));
+        for (int i = 2; i < num; i++) {
+            fibonacci.add(fibonacci.get(i - 1) + fibonacci.get(i - 2));
         }
         System.out.println(fibonacci);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        fibonacci(num);
     }
 }
