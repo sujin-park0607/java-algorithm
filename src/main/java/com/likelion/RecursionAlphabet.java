06a2b66d560d12536a5fadd4623ea1a78b541834
sujin0607 <goweg0607@gmail.com>
2022-11-28 10:03:39 +0900

    feat: 재귀 사용 알파벳 두글자 출력

--- src/main/java/com/likelion/RecursionAlphabet.java
+++ src/main/java/com/likelion/RecursionAlphabet.java
@@ -7,7 +7,19 @@ public class RecursionAlphabet {
         System.out.println((char)n);
     }
 
+    public static void doubleAlphabet(int n){
+        if(n < 65) return;
+        doubleAlphabet(n-1);
+        doubleAlphabet2(n,n);
+    }
+
+    public static void doubleAlphabet2(int n, int n2){
+        if(n < 65) return;
+        doubleAlphabet2(n-1,n2);
+        System.out.printf("%c%c\n",n2,n);
+    }
+
     public static void main(String[] args) {
-        alphabet(90);
+        doubleAlphabet(90);
     }
 }
