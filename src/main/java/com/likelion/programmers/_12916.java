36cdaaff7d347ca9380a812824d3bdd15a856d39
sujin0607 <goweg0607@gmail.com>
2022-11-30 10:16:37 +0900

    feat: programmers 12916 구현

---
+++ src/main/java/com/likelion/programmers/_12916.java
@@ -0,0 +1,24 @@
+package com.likelion.programmers;
+
+import java.util.Scanner;
+
+public class _12916 {
+    public static boolean pOrY(String input){
+        String[] inputArr = input.split("");
+        int result = 0;
+        for (String s : inputArr) {
+            if(s.equals("p") || s.equals("P")) result++;
+            else if (s.equals("y") || s.equals("Y")) result --;
+            else continue;
+        }
+
+        if(result==0) return true;
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(pOrY(input));
+    }
+}
