56ce98514fe5edc921bcac2eada1b8f4203e9f11
sujin-park0607 <goweg0607@gmail.com>
2022-12-08 10:58:39 +0900

    refactor: 재귀없이 dp를 사용한 피본나치수열

--- src/main/java/com/likelion/FibonacciDP.java
+++ src/main/java/com/likelion/FibonacciDP.java
@@ -3,19 +3,16 @@ package com.likelion;
 import java.util.Scanner;
 
 public class FibonacciDP {
-    static int[] dp = new int[1000];
-    static int fibonacci(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(dp[n] != 0) return dp[n];
-        dp[n] = fibonacci(n - 2) + fibonacci(n - 1);
-        return dp[n];
-    }
-
     public static void main(String[] args){
+
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        System.out.println(fibonacci(N));
-    }
+        int[] dp = new int[N + 1];
+        dp[1] = 1;
 
+        for(int i = 2;i <= N;i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        System.out.println(dp[N]);
+
+    }
 }
