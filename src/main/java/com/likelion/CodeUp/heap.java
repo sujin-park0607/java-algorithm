cfb8ad2e0dc0ed35539c591011b676920835760a
sujin-park0607 <goweg0607@gmail.com>
2022-12-08 10:51:18 +0900

    feat: 6 * 7 배열 만들기

--- src/main/java/com/likelion/CodeUp/heap.java
+++ src/main/java/com/likelion/CodeUp/heap.java
@@ -5,38 +5,38 @@ import java.util.Arrays;
 
 
 public class heap {
-    public void heap(int[] arr, int parentIdx, int lastIdx){
-        int left = parentIdx * 2 + 1;
-        int right = parentIdx * 2 + 2;
-        int maxIdx = parentIdx;
-
-        if(left < lastIdx && arr[lastIdx] < arr[left]){
-            maxIdx = left;
-        }
-
-        if(right < lastIdx && arr[maxIdx] < arr[right]){
-            maxIdx = right;
-        }
-
-        if(parentIdx != maxIdx){
-            int[] sortArr = swap(arr, maxIdx, parentIdx);
-            heap(sortArr, maxIdx, lastIdx);
-
-        }
-
-    }
-
-    private int[] swap(int[] arr, int maxIdx, int parentIdx) {
-        int temp = arr[maxIdx];
-        arr[maxIdx] = arr[parentIdx];
-        arr[parentIdx] = temp;
-        return arr;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{6, 5, 7, 8};
-
-        heap(arr, 6);
-
-    }
+//    public void heap(int[] arr, int parentIdx, int lastIdx){
+//        int left = parentIdx * 2 + 1;
+//        int right = parentIdx * 2 + 2;
+//        int maxIdx = parentIdx;
+//
+//        if(left < lastIdx && arr[lastIdx] < arr[left]){
+//            maxIdx = left;
+//        }
+//
+//        if(right < lastIdx && arr[maxIdx] < arr[right]){
+//            maxIdx = right;
+//        }
+//
+//        if(parentIdx != maxIdx){
+//            int[] sortArr = swap(arr, maxIdx, parentIdx);
+//            heap(sortArr, maxIdx, lastIdx);
+//
+//        }
+//
+//    }
+//
+//    private int[] swap(int[] arr, int maxIdx, int parentIdx) {
+//        int temp = arr[maxIdx];
+//        arr[maxIdx] = arr[parentIdx];
+//        arr[parentIdx] = temp;
+//        return arr;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = new int[]{6, 5, 7, 8};
+//
+//        heap(arr, 6);
+//
+//    }
 }
--- src/main/java/com/likelion/LCS.java
+++ src/main/java/com/likelion/LCS.java
@@ -1,5 +1,10 @@
 package com.likelion;
 
 public class LCS {
+    public static void main(String[] args) {
+        String str1 = "ABCDCBA";
+        String str2 = "DCABDC";
 
+        int[][] arr = new int[str2.length()][str1.length()];
+    }
 }
