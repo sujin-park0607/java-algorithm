fd303415d32843e527a1a9943f08c4d9bd40d4d5
sujin-park0607 <goweg0607@gmail.com>
2022-12-09 09:44:11 +0900

    feat: LCS 구현중

--- src/main/java/com/likelion/LCS.java
+++ src/main/java/com/likelion/LCS.java
@@ -9,7 +9,22 @@ public class LCS {
 
         for( int i=0; i<str2.length(); i++){
             for(int j=0; j<str1.length(); j++){
-                System.out.printf("i:%s j:%s\n");
+                System.out.printf("i:%s j:%s\n", str2.charAt(i), str1.charAt(j));
+
+                //두개가 같으면 대각선
+                if(str2.charAt(i) == str1.charAt(j)){
+                    arr[j][i] =  arr[j-1][i-1];
+                    System.out.println(arr[j][i]);
+
+                //두개가 다르면 위,왼쪽 비교해서 max 사용
+                }else{
+                    if(arr[j-1][i] > arr[j][i-1]){
+                        arr[j][i] =  arr[j-1][i];
+                    }else{
+                        arr[j][i] =  arr[j][i-1];
+                    }
+                }
+
             }
         }
 
