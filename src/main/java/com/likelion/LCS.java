7b1a3bd88fff5d8c2083da1a22d2dfa1c669e4ae
sujin-park0607 <goweg0607@gmail.com>
2022-12-09 09:37:20 +0900

    feat: LCS 구현중

--- src/main/java/com/likelion/LCS.java
+++ src/main/java/com/likelion/LCS.java
@@ -5,12 +5,14 @@ public class LCS {
         String str1 = "ABCDCBA";
         String str2 = "DCABDC";
 
-        int[][] arr = new int[str2.length()][str1.length()];
+        int[][] arr = new int[str2.length()][str1.length()+1];
 
         for( int i=0; i<str2.length(); i++){
             for(int j=0; j<str1.length(); j++){
                 System.out.printf("i:%s j:%s\n");
             }
         }
+
+
     }
 }
