d163d8e4fe70b29869c9b7c29e58a6976ca141c4
sujin-park0607 <goweg0607@gmail.com>
2022-12-12 09:47:10 +0900

    feat: 첫째 행만 있는 경우

--- src/main/java/com/likelion/DpMinCost.java
+++ src/main/java/com/likelion/DpMinCost.java
@@ -1,15 +1,23 @@
 package com.likelion;
 
+import java.util.Arrays;
+
 public class DpMinCost {
     public static void getMinCost(int[][] arr){
         int[][] dp = new int[arr.length][arr.length];
+
+        //첫째칸만 있는 경우
         for(int i=0; i<arr.length; i++){
             for(int j=0; j<arr.length; j++){
                 if(i==0 && j==0){
                     dp[i][j] = arr[i][j];
+                }else if(i == 0 && j>0){
+                    dp[i][j] = dp[i][j-1] + arr[i][j];
                 }
             }
+            System.out.println(Arrays.toString(dp[i]));
         }
+
     }
 
     public static void main(String[] args) {
