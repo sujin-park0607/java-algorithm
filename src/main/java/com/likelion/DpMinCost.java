f134186499a04ba08da6eebc9db963eaac4137eb
sujin-park0607 <goweg0607@gmail.com>
2022-12-12 09:38:54 +0900

    feat: 첫째칸만 있는 경우

--- src/main/java/com/likelion/DpMinCost.java
+++ src/main/java/com/likelion/DpMinCost.java
@@ -2,8 +2,14 @@ package com.likelion;
 
 public class DpMinCost {
     public static void getMinCost(int[][] arr){
-
         int[][] dp = new int[arr.length][arr.length];
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(i==0 && j==0){
+                    dp[i][j] = arr[i][j];
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
