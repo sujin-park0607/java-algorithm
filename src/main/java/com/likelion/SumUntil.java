7d580b1ed8af02cb41afc0c75809c742579a45e9
sujin-park0607 <goweg0607@gmail.com>
2022-12-14 09:29:24 +0900

    feat: from부터 끝까지 더하기

--- src/main/java/com/likelion/SumUntil.java
+++ src/main/java/com/likelion/SumUntil.java
@@ -1,5 +1,7 @@
 package com.likelion;
 
+import java.util.Arrays;
+
 public class SumUntil {
     public static void main(String[] args) {
         int[] coins = {2, 7, 40, 19};
@@ -7,27 +9,14 @@ public class SumUntil {
 
         //coin이 하나만 있는 경우
         //2 or 7 or 20 or 19
-        for (int i=0; i<coins.length; i++){
-            dp[i][i] = dp[coins[i]][0];
-        }
-
-        //coin이 두개 있는 경우
-        for (int i=0; i<coins.length-1; i++){
-            int left = Math.max(coins[i], coins[i+1]);
-            int right = Math.min(coins[i], coins[i+1]);
-
-            dp[i][i+1] = dp[left][right];
-        }
-
-        //coin이 하나만 있는 경우
-        //2 or 7 or 20 or 19
-        for(int i=0; i<coins.length-1; i++){
-            int sum = 0;
-            for(int j=i; j<coins.length-1; j++){
-                sum += dp[i][j];
-                dp[i][j] = sum;
-                System.out.println(sum);
+        int i = 0;
+        for(int j=0; i<coins.length; j++){
+            if(j==0){
+                dp[i][j] = coins[0];
+            }else{
+                dp[i][j] = dp[i][j-1] + coins[j];
             }
+            System.out.println(Arrays.toString(dp[i]));
         }
     }
 }
